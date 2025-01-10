db48e831cf8ad44e92530888ce6c3d552de10134
Mujtaba Saqib <165723415+mujtabasaqib19@users.noreply.github.com>
2025-01-10 23:13:05 +0500

    Update resume.py

--- Gemini Projects/ResumeTrackingSystem/resume.py
+++ Gemini Projects/ResumeTrackingSystem/resume.py
@@ -7,13 +7,10 @@ import fitz  # PyMuPDF
 import io
 import base64
 
-# Load environment variables
 load_dotenv()
 
-# Configure Google Gemini API
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-# Function to generate response using Google Gemini API
 def generate_response(input, pdf_content, prompt):
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content([input, pdf_content[0], prompt])
@@ -23,11 +20,9 @@ def generate_response(input, pdf_content, prompt):
 def input_pdf(uploaded_file):
     pdf_document = fitz.open(stream=uploaded_file.read(), filetype="pdf")
     first_page = pdf_document[0]  # Get the first page of the PDF
-    
-    # Convert the first page to a Pixmap (image object)
+
     pixmap = first_page.get_pixmap()
-    
-    # Save the pixmap to a byte array
+
     img_byte_arr = io.BytesIO()
     img = Image.frombytes("RGB", [pixmap.width, pixmap.height], pixmap.samples)
     img.save(img_byte_arr, format="JPEG")
@@ -53,7 +48,7 @@ uploaded_file = st.file_uploader("Upload your resume in PDF format", type=["pdf"
 if uploaded_file is not None:
     st.write("PDF uploaded successfully.")
 
-# Buttons
+
 s1 = st.button("Tell Me About the Resume")
 s2 = st.button("Percentage match")
 
@@ -93,97 +88,3 @@ elif s2:
     else:
         st.write("Please upload the resume.")
 
-
-
-
-
-
-
-
-# from dotenv import load_dotenv
-# import streamlit as st
-# import google.generativeai as genai
-# import os
-# from PIL import Image
-# import pdf2image
-# import io
-# import base64
-
-# load_dotenv()
-
-# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-# def generate_response(input, pdf_content, prompt):
-#     model = genai.GenerativeModel("gemini-pro-vision")
-#     response = model.generate_content([input, pdf_content[0], prompt])
-#     return response.text
-
-# def input_pdf(uploaded_file):
-#     images = pdf2image.convert_from_bytes(uploaded_file.read())
-#     first_page = images[0]
-
-#     if first_page is not None:
-#         img_byte_arr = io.BytesIO()
-#         first_page.save(img_byte_arr, format="JPEG")  
-#         img_byte_arr = img_byte_arr.getvalue() 
-        
-#         # Prepare the encoded data in the required format
-#         pdf_parts = [
-#             {
-#                 "mime_type": "image/jpeg",
-#                 "data": base64.b64encode(img_byte_arr).decode()  # Encode to base64 and decode to a string
-#             }
-#         ]
-#         return pdf_parts
-#     else:
-#         raise FileNotFoundError("No first page found to convert.")
-
-# # Streamlit App
-# st.set_page_config(page_title="ATS Resume")
-# st.header("Resume Application Tracking System (ATS) Using Google Gemini Pro Vision")
-
-# # Inputs
-# job_description = st.text_area("Job Description", key="input")
-# uploaded_file = st.file_uploader("Upload your resume in PDF format", type=["pdf"])
-
-# if uploaded_file is not None:
-#     st.write("PDF uploaded successfully.")
-
-# # Buttons
-# s1 = st.button("Tell Me About the Resume")
-# s2 = st.button("Percentage match")
-
-# # Prompts
-# input_prompt1 = """
-# You are an experienced HR with tech experience in the field of Data Science, Full Stack Web Development, 
-# Big Data Engineering, DEVOPS, and Data Analysis. Your task is to review the provided resume 
-# against the job description for these profiles. 
-# Please share your professional evaluation on whether the candidate's profile aligns with the specified role. Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
-# """
-
-# input_prompt2 = """
-# You are a skilled ATS (Applicant Tracking System) scanner with a deep understanding of Data Science, 
-# Web Development, Big Data Engineering, DEVOPS, Data Analysis, and deep ATS functionality. 
-# Your task is to evaluate the resume against the provided job description. 
-# Give me the percentage match of the resume with the job description. First, the output should come as a percentage, followed by the keywords missing and a professional evaluation.
-# """
-
-# if s1:
-#     st.write("You clicked on Tell Me About the Resume")
-#     if uploaded_file is not None:
-#         pdf_content = input_pdf(uploaded_file)
-#         result = generate_response(job_description, pdf_content, input_prompt1)
-#         st.subheader("The response is:")
-#         st.write(result)
-#     else:
-#         st.write("Please upload the resume.")
-
-# elif s2:
-#     st.write("You clicked on Percentage match ")
-#     if uploaded_file is not None:
-#         pdf_content = input_pdf(uploaded_file)
-#         result = generate_response(job_description, pdf_content, input_prompt2)
-#         st.subheader("The response is:")
-#         st.write(result)
-#     else:
-#         st.write("Please upload the resume.")
